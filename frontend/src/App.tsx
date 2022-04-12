ebe4f1871662797da330589b8cfaf1292d5b68e1
Ihor Tarkhan <ihor.tarkhan.working@gmail.com>
2022-04-12 16:15:20 +0300

    clean up code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,44 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  root: {
+    textAlign: "center",
+  },
+  header: {
+    backgroundColor: "#282c34",
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: "calc(10px + 2vmin)",
+    color: "white",
+    margin: -8,
+  },
+  logo: {
+    height: "40vmin",
+    pointerEvents: "none",
+  },
+  link: {
+    color: "#61dafb",
+  },
+});
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={"images/logo.svg"} className="App-logo" alt="logo" />
+    <div className={classes.root}>
+      <header className={classes.header}>
+        <img alt={"logo"} src={"images/logo.svg"} className={classes.logo} />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className={classes.link}
+          href={"https://reactjs.org"}
+          target={"_blank"}
+          rel={"noopener noreferrer"}
         >
           Learn React
         </a>
