365ddb79859d45f9bda7fa3ebea780d30621819a
Ihor Tarkhan <ihor.tarkhan.working@gmail.com>
2022-04-17 11:42:56 +0300

    keycloak

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,40 +1,19 @@
 import React from "react";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav";
+import WelcomePage from "./pages/Homepage";
+import SecuredPage from "./pages/Securedpage";
 
-export const App = () => (
-  <BrowserRouter>
+export const App = () => {
+  return (
     <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/users">Users</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <Routes>
-        <Route path="/about" element={<About />} />
-        <Route path="/users" element={<Users />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Nav />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<WelcomePage />} />
+          <Route path="/secured" element={<SecuredPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  </BrowserRouter>
-);
-
-const Home = () => {
-  return <h2>Home</h2>;
-};
-
-const About = () => {
-  return <h2>About</h2>;
-};
-
-const Users = () => {
-  return <h2>Users</h2>;
+  );
 };
