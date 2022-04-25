bd88e370d7c022dcc5ff663dd87c6654d92372f2
Ihor Tarkhan <ihor.tarkhan.working@gmail.com>
2022-04-25 09:33:19 +0300

    cors

---
+++ servlet/src/main/java/com/ihortarkhan/ooplab61/servlets/OptionsServlet.java
@@ -0,0 +1,15 @@
+package com.ihortarkhan.ooplab61.servlets;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/*")
+public class OptionsServlet extends HttpServlet {
+    @Override
+    protected void doOptions(HttpServletRequest request, HttpServletResponse response) {
+        response.addHeader("Access-Control-Allow-Headers", "Authorization");
+        response.setStatus(HttpServletResponse.SC_ACCEPTED);
+    }
+}
